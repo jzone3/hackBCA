a8e65c26f2dc4a1f0a5daef5e640c9042e63c789
Victor Lourng <hello@victorlourng.com>
2014-02-21 23:45:41 -0500

    A few fixes.

--- scripts/app.js
+++ scripts/app.js
@@ -16,3 +16,6 @@ return(!i||i!==r&&!b.contains(r,i))&&(e.type=o.origType,n=o.handler.apply(this,a
       $('footer blockquote small').html( data.author );
     });
 })();
+
+// Safari Webapp
+(function(a,b,c){if(c in b&&b[c]){var d,e=a.location,f=/^(a|html)$/i;a.addEventListener("click",function(a){d=a.target;while(!f.test(d.nodeName))d=d.parentNode;"href"in d&&(d.href.indexOf("http")||~d.href.indexOf(e.host))&&(a.preventDefault(),e.href=d.href)},!1)}})(document,window.navigator,"standalone")
